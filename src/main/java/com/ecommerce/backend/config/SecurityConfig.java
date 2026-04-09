6b67885952c9d615783fc6ca80cc22fbf20ec32a
ameycode88 <146335383+ameycode88@users.noreply.github.com>
2026-04-09 15:20:09 +0530

    patch

--- src/main/java/com/ecommerce/backend/config/SecurityConfig.java
+++ src/main/java/com/ecommerce/backend/config/SecurityConfig.java
@@ -95,7 +95,9 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+        configuration.setAllowedOrigins(List.of("http://localhost:3000",
+                "https://e-commerce-frontend-xi-pearl.vercel.app"
+        ));
         configuration.setAllowedMethods(
                 List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
